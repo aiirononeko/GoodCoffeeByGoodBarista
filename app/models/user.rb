948c34e7b277554afe3030315d0366eb715c3543
片田凌太 <katadaryouta@katadaryoutanoMacBook-Pro.local>
2019-11-20 12:29:14 +0900

    バリスタユーザー登録機能を実装

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :sex, presence: true
+  validates :birthday, presence: true
+
+  has_one_attached :image
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    shared: 'shared/error_messages'
+  }
+  resources :users, only: [:index, :show]
 end
